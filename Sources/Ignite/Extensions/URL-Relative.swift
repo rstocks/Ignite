d175ffc37176e77ddc5165fe6afe49400f9d5b75
Richard Stocks <richard@stocksfamily.org>
2024-06-05 14:13:24 +1000

    Linux test 2

--- Sources/Ignite/Extensions/URL-Relative.swift
+++ Sources/Ignite/Extensions/URL-Relative.swift
@@ -12,8 +12,10 @@ extension URL {
     /// - Parameter other: The base URL to compare against.
     /// - Returns: A relative URL.
     func relative(to other: URL) -> String {
-        let basePath = other.path()
-        let thisPath = path()
+        let basePath = other.path
+        let thisPath = path
+//        let basePath = other.path()
+//        let thisPath = path()
         let result = thisPath.trimmingPrefix(basePath)
 
         return String(result)
